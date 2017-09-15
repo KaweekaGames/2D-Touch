5535a79af5ed49b9dfa5297b3c1b55ae7b85072e
David Bibb <kaweekagames@gmail.com>
2017-09-14 21:18:03 -0700

    initial commit

---
+++ 2D Touch/Assets/CameraPosition.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPosition : MonoBehaviour
+{
+    public GameObject player;
+    public float offsetX = 9f;
+    public float offsetY = 6f;
+
+    private Transform playerTrans;
+    private Vector3 oldPos;
+    private Vector3 newPos;
+    private Vector3 playerPos;
+    private float transitionSpeed = 5f;
+    private float Zangle;
+    private float newX;
+    private float newY;
+    private Camera camera = new Camera();
+
+    // Use this for initialization
+    void Awake()
+    {
+
+        //camera = gameObject.GetComponent<Camera>();
+        camera = Camera.main;
+
+    }
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerTrans = player.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (player != null)
+        {
+            playerPos = playerTrans.position;
+            Zangle = playerTrans.eulerAngles.z;
+            oldPos = transform.position;
+
+            if (Zangle >= 0 && Zangle < 90f)
+            {
+                newX = -(offsetX * Zangle / 90f);
+                newY = (offsetY - (offsetY * Zangle / 90f));
+                newPos = new Vector3(Mathf.Lerp(oldPos.x, playerPos.x + newX, Time.deltaTime * transitionSpeed), Mathf.Lerp(oldPos.y, playerPos.y + newY, Time.deltaTime * transitionSpeed), oldPos.z);
+            }
+
+            if (Zangle >= 90f && Zangle < 180f)
+            {
+                newX = -(offsetX - (offsetX * (Zangle - 90f) / 90f));
+                newY = -(offsetY * (Zangle - 90f) / 90f);
+                newPos = new Vector3(Mathf.Lerp(oldPos.x, playerPos.x + newX, Time.deltaTime * transitionSpeed), Mathf.Lerp(oldPos.y, playerPos.y + newY, Time.deltaTime * transitionSpeed), oldPos.z);
+            }
+
+            if (Zangle >= 180f && Zangle < 270f)
+            {
+                newX = (offsetX * (Zangle - 180f) / 90f);
+                newY = -(offsetY - (offsetY * (Zangle - 180f) / 90f));
+                newPos = new Vector3(Mathf.Lerp(oldPos.x, playerPos.x + newX, Time.deltaTime * transitionSpeed), Mathf.Lerp(oldPos.y, playerPos.y + newY, Time.deltaTime * transitionSpeed), oldPos.z);
+            }
+
+            if (Zangle >= 270f && Zangle < 360f)
+            {
+                newX = (offsetX - (offsetX * (Zangle - 270f) / 90f));
+                newY = (offsetY * (Zangle - 270f) / 90f);
+                newPos = new Vector3(Mathf.Lerp(oldPos.x, playerPos.x + newX, Time.deltaTime * transitionSpeed), Mathf.Lerp(oldPos.y, playerPos.y + newY, Time.deltaTime * transitionSpeed), oldPos.z);
+            }
+            transform.position = newPos;
+        }
+    }
+}
---
+++ 2D Touch/Assets/Movement.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public GameObject player;
+    public float rotSpeed;
+    public float forwardSpeed;
+    public float forwardSpeedFactor;
+    public float maxSpeed;
+    public float forwardSlowDownFactor;
+
+    private float rotAmount = 0;
+    private float playerSpeed = 0;
+    private Vector3 velocity;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Quaternion rot = player.transform.rotation;
+
+        float z = rot.eulerAngles.z;
+
+        z -= rotAmount * rotSpeed * Time.deltaTime;
+
+        rot = Quaternion.Euler(0, 0, z);
+
+        player.transform.rotation = rot;
+
+
+        Vector3 pos = player.transform.position;
+
+        velocity = new Vector3(0, playerSpeed * forwardSpeedFactor * Time.deltaTime, 0);
+
+        pos += rot * velocity;
+
+        player.transform.position = pos;
+
+
+        if (rotAmount > 0)
+        {
+            rotAmount -= Time.deltaTime;
+        }
+        else if(rotAmount < 0)
+        {
+            rotAmount += Time.deltaTime;
+        }
+
+        if(playerSpeed > 0)
+        {
+            playerSpeed -= Time.deltaTime * forwardSlowDownFactor;
+        }
+        //else if (playerSpeed < 0)
+        //{
+        //   playerSpeed += Time.deltaTime;
+        //}
+
+        playerSpeed = Mathf.Clamp(playerSpeed, 0, maxSpeed);
+	}
+
+    public void Clockwise()
+    {
+        rotAmount = rotAmount + 1;
+    }
+
+    public void CounterClockwise()
+    {
+        rotAmount = rotAmount - 1;
+    }
+
+    public void Forward()
+    {
+        playerSpeed += 1;
+    }
+}
